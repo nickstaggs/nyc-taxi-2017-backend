ec7031b801074c4ffdeb12eb146fa6fdf4e595e5
Nick Staggs <nick.c.staggs@gmail.com>
2018-09-23 17:25:41 -0400

    Removed get all routes mapping and added sql updates to zones

---
+++ src/main/db/updateZonesLibertyEllisGovernorsIslands.sql
@@ -0,0 +1,3 @@
+UPDATE zones SET zone = 'Liberty Island' WHERE location_id = 103;
+UPDATE zones SET zone = 'Ellis Island' WHERE location_id = 104;
+UPDATE zones SET zone = 'Governors Island' WHERE location_id = 105;
